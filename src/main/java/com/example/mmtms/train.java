9c233a31defb7d622ae15633c9f5e7b862bda7f2
deysur <surajitdey000@gmail.com>
2024-01-30 10:39:05 +0530

    add train module

---
+++ src/main/java/com/example/mmtms/train.java
@@ -0,0 +1,11 @@
+package com.example.mmtms;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+
+public class flights {
+    @GetMapping ("/train")
+    public String getData() { return "redirect page to IRCTC for booking train" ; }
+
+
+}
